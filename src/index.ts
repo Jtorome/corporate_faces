fe3bd4ca6ab78d5d62a539b958838c0723a9518a
Juan Alberto Toro Mejia <juanalbtoro@hotmail.com>
2023-06-16 16:42:27 -0500

    prettier fixes

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { config } from './config/config';
 import { createApp } from './app';
 
 createApp().listen(config.port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Corriendo en el puerto ${config.port}`);
 });
--- src/middlewares/error.handler.ts
+++ src/middlewares/error.handler.ts
@@ -4,6 +4,7 @@ import { failure } from '../utils/response';
 
 const logErrors = (error: Error, req: Request, res: Response, next: NextFunction) => {
   //TODO: redireccionar a la pagina para atrapar errores
+  // eslint-disable-next-line no-console
   console.error(error);
   next(error);
 };
